a293614756f25a2af14fa0e3c2778b373294d218
Muddasir khuwaja <engr.muddasir01@gmail.com>
2026-02-23 11:26:21 +0100

    Add files via upload

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/muddasir_khuwaja. (3).pdf";
+import thumbnail from "../../Assets/WALLPAPER.png";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            download
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume" style={{ justifyContent: "center", marginTop: "30px" }}>
+          <img src={thumbnail} alt="CV Thumbnail" style={{ maxWidth: "400px", borderRadius: "12px", boxShadow: "0 4px 16px rgba(0,0,0,0.2)" }} />
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative", marginTop: "30px" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            download
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
